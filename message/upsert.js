9101f6908a56c1bf581c363e9523be29bcd0f458
godinky <125708718+godinky@users.noreply.github.com>
2023-07-29 17:42:35 -0400

    Update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -61,8 +61,8 @@ break
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-  fs.unwatchFile(file)
-  console.log(`Update ${__filename}`)
-  delete require.cache[file]
-  require(file)
+	fs.unwatchFile(file)
+	console.log(`Update ${__filename}`)
+	delete require.cache[file]
+	require(file)
 })
